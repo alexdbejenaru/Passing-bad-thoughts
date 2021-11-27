2437b7e2324423c05c8992e370d4534dbc7ce1ee
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2021-11-27 22:44:21 +0200

    finished functionality for the project

---
+++ src/components/AddThoughtForm.js
@@ -0,0 +1,39 @@
+import { generateId, getNewExpirationTime } from "../utilities";
+import { useState } from "react";
+
+const AddThoughtForm = (props) => {
+
+    const [ text, setText ] = useState('');
+    const handleTextChange = ( event ) => {
+        setText(event.target.value);
+    };
+    const handleSubmit = ( event ) => {
+        event.preventDefault();
+        const thought = {
+            id: generateId(),
+            text: text,
+            expiresAt: getNewExpirationTime(),
+        };
+
+        if ( text.length > 0 ) {
+            props.addThought(thought);
+            setText('');
+        }
+    }
+    return ( 
+        <>
+            <form className="AddThoughtForm" onSubmit={handleSubmit}>
+                <input
+                    value={text}
+                    onChange={handleTextChange}
+                    type="text"
+                    aria-label="What's on your mind?"
+                    placeholder="What's on your mind?"
+                />
+                <input type="submit" value="Add"  />
+            </form>
+        </>
+     );
+}
+ 
+export default AddThoughtForm;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,48 @@
 import "../styles/index.scss";
+import { generateId, getNewExpirationTime } from '../utilities';
+import AddThoughtForm from './AddThoughtForm';
+import Thought from './Thought';
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [ thoughts, setThoughts ] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing BAD thoughts',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: 'They will be removed after 15 seconds so you can forget about them',
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = ( thought ) => {
+    setThoughts(( prev ) => [thought, ...prev]);
+  };
+
+  const removeThought = ( thoughtIdToRemove ) => {
+    setThoughts(( prev ) => prev.filter(( thought ) => thought.id !== thoughtIdToRemove));
+  };
+
   return (
-    <>
-    </>
+    <main>
+      <section className="app-container">
+        <header>
+          <h1>Forget your bad thoughts</h1>
+        </header>
+        <section>
+          <AddThoughtForm addThought={addThought} />
+          <ul className="thoughts">
+            {thoughts.map((thought) => (
+              <Thought key={thought.id} thought={thought} removeThought={removeThought} />
+            ))}
+          </ul>
+        </section>
+      </section>
+    </main>
   );
 };
 
